1808f4b84f709f1e1b3d5be9876e34670af76bd6
yui <asd25202002@gmail.com>
2022-07-27 21:58:37 +0800

    修正願望清單頁面
    沒有願望清單時的顯示頁面更新

--- src/views/UserfavonriteViews.vue
+++ src/views/UserfavonriteViews.vue
@@ -1,36 +1,41 @@
 <template>
-  <table class="table table-hover align-middle">
-    <thead>
-      <tr>
-        <th class="text-center">圖片</th>
-        <th class="text-center">分類</th>
-        <th>產品名稱</th>
-        <th class="text-right">原價</th>
-        <th class="text-right">售價</th>
-        <th class="text-center">編輯</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="product in products" :key="product.id">
-        <td class="text-center">
-          <img :src="product.imageUrl" :alt="product.title"
-          class="img-fluid cartImg-size">
-        </td>
-        <td class="text-center">{{ product.category }}</td>
-        <td>{{ product.title }}</td>
-        <td class="text-right">{{ $filters.currency(product.origin_price) }}</td>
-        <td class="text-right">{{ $filters.currency(product.price) }}</td>
-        <td class="text-center">
-          <div class="btn-group">
-            <button type="button" class="btn btn-danger"
-            @click.prevent="removeFavorite(product)">
-              <i class="bi bi-heart"></i>
-            </button>
-          </div>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <main>
+    <div class="container">
+      <table class="table table-hover align-middle" v-if="myFavorite.length">
+        <thead>
+          <tr>
+            <th class="text-center">圖片</th>
+            <th class="text-center">分類</th>
+            <th>產品名稱</th>
+            <th class="text-right">原價</th>
+            <th class="text-right">售價</th>
+            <th class="text-center">編輯</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="product in products" :key="product.id">
+            <td class="text-center">
+              <img :src="product.imageUrl" :alt="product.title"
+              class="img-fluid cartImg-size">
+            </td>
+            <td class="text-center">{{ product.category }}</td>
+            <td>{{ product.title }}</td>
+            <td class="text-right">{{ $filters.currency(product.origin_price) }}</td>
+            <td class="text-right">{{ $filters.currency(product.price) }}</td>
+            <td class="text-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-danger"
+                @click.prevent="removeFavorite(product)">
+                  <i class="bi bi-heart"></i>
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="py-4 fs-5 text-muted" v-else>您還沒有願望清單</div>
+    </div>
+  </main>
 </template>
 
 <script>
